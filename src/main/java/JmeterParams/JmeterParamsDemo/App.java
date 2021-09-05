3a737ba4bb415974a8b0608f199b620641f35825
Haritha Manikonda <haritha.manikonda@gmail.com>
2021-09-04 22:21:15 -0700

    first commit

---
+++ src/main/java/JmeterParams/JmeterParamsDemo/App.java
@@ -0,0 +1,13 @@
+package JmeterParams.JmeterParamsDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
